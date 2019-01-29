9f5ea95f36cec534d9eb20435009c1e4757dbe6c
alsamzu <zulfalsam@gmail.com>
2019-01-29 21:06:35 +0300

    Story Activity

--- app/src/main/java/com/example/planetsfacts/model/Story.java
+++ app/src/main/java/com/example/planetsfacts/model/Story.java
@@ -1,4 +1,30 @@
 package com.example.planetsfacts.model;
 
+import com.example.planetsfacts.R;
+
 public class Story {
+    private Page [] pages;
+
+    public Story(){
+        pages = new Page[8];
+
+        pages[0]= new Page(R.string.page0);
+        pages[1]= new Page(R.string.page1);
+        pages[2]= new Page(R.string.page2);
+        pages[3]= new Page(R.string.page3);
+        pages[4]= new Page(R.string.page4);
+        pages[5]= new Page(R.string.page5);
+        pages[6]= new Page(R.string.page6);
+        pages[7]= new Page(R.string.page7);
+
+
+    }
+
+    public Page getPage(int pageNumber) {
+        if (pageNumber >= pages.length) {
+            pageNumber = 0;
+        }
+        return pages[pageNumber];
+    }
+
 }
--- app/src/main/java/com/example/planetsfacts/ui/StoryActivity.java
+++ app/src/main/java/com/example/planetsfacts/ui/StoryActivity.java
@@ -2,14 +2,37 @@ package com.example.planetsfacts.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.planetsfacts.R;
+import com.example.planetsfacts.model.Page;
+import com.example.planetsfacts.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
+        private Story story;
 
+        private TextView storytextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        storytextview = (TextView) findViewById(R.id.StoryTextView);
+
+        story = new Story();
+
+
+    }
+
+    public void loadContent(int pageNumber){
+        final Page  page = story.getPage(pageNumber);
+
+        String pageText = getString(page.getTextId());
+        //pageText = String.format(pageText);
+
+        //ADD NAME IF PLACEHOLDER IS INCLUDED WONT ADD IF NOT
+        storytextview.setText(pageText);
+
+
     }
 }
